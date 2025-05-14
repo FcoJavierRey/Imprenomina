d99d2fbb208f18625c7f85d0e26009cb6322c586
FcoJavierRey <frey.inerza.com>
2025-05-14 14:04:32 +0200

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('Login', ()=>{
+    it('Login de usuario'), ()=>{
+        cy.visit('https://intranetpre.educacion.org/educacion/certificados/imprenominas/Scripts/login.asp'),
+        cy.get('#username').type('ext.freygom'),
+        cy.get('#password').type('Fer1123@'),
+        cy.get('#boton-login').click(),
+        cy.wait(3000), 
+        cy.get('.Titular-doc').should('exist')
+    }
+})
